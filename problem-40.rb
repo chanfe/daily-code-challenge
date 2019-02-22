071df05bb4318ddc570de12d776295e30b299ef1
Felix Chan <felixchan29@gmail.com>
2019-02-22 18:03:14 -0500

    finished bracket

---
+++ problem-40.rb
@@ -0,0 +1,23 @@
+#Given an array of integers where every integer occurs three times except for one integer,
+#which only occurs once, find and return the non-duplicated integer.
+
+#For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+
+#Do this in O(N) time and O(1) space.
+
+def non_dup_finder(arr)
+  hash = {}
+  answer
+  for i in arr
+    # if hash[i]
+    #   hash[i] = hash[i] + 1
+    # else
+    #   hash[i] = 1
+    # end
+    # ???? 
+  end
+
+end
+
+arr1 = [6, 1, 3, 3, 3, 6, 6]
+arr2 = [13, 19, 13, 13]
