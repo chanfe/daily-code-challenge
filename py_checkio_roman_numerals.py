55d7f226d0c206325e202fac5b429624e8fc4054
Felix Chan <felixchan29@gmail.com>
2019-02-28 16:20:31 -0500

    modlues

--- py_checkio_roman_numerals.py
+++ py_checkio_roman_numerals.py
@@ -37,7 +37,8 @@ def checkio(data):
     #my_dict.items()[0]    -> (key, value) tuple of "first" element
 
     remainding_number = data
-    test = {1000 : 'M', 900 : 'CM', 500 : 'D', 400 : 'CD', 100 : 'C', 90 : 'XC', 50 : 'L', 40 : 'XL', 10 : 'X', 9 : 'IX', 5 : 'V', 4 : 'IV', 1 : 'I'}
+    test = {1000 : 'M', 900 : 'CM', 500 : 'D', 400 : 'CD', 100 : 'C', 90 : 'XC',
+        50 : 'L', 40 : 'XL', 10 : 'X', 9 : 'IX', 5 : 'V', 4 : 'IV', 1 : 'I'}
     roman_numeral = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     answer = ""
     count = 0
