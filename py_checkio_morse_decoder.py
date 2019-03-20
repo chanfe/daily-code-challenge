5a2c798379cc7ac06414e457c73b6be023d07423
Felix Chan <felixchan29@gmail.com>
2019-03-20 12:27:31 -0400

    morse decoder

---
+++ py_checkio_morse_decoder.py
@@ -0,0 +1,55 @@
+# Your task is to decrypt the secret message using the Morse code.
+# The message will consist of words with 3 spaces between them and 1 space between each letter of each word.
+# If the decrypted text starts with a letter then you'll have to print this letter in uppercase.
+#
+# example
+#
+# Input: The secret message.
+#
+# Output: The decrypted text.
+#
+# Example:
+#
+# morse_decoder("... --- -- .   - . -..- -") == "Some text"
+# morse_decoder("..--- ----- .---- ---..") == "2018"
+# morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--") == "It was a good day"
+# How it is used: For cryptography and spy work.
+#
+# Precondition:
+# 0 < len(message) < 100
+# The message will consists of numbers and English letters only.
+
+MORSE = {'.-':    'a', '-...':  'b', '-.-.':  'c',
+         '-..':   'd', '.':     'e', '..-.':  'f',
+         '--.':   'g', '....':  'h', '..':    'i',
+         '.---':  'j', '-.-':   'k', '.-..':  'l',
+         '--':    'm', '-.':    'n', '---':   'o',
+         '.--.':  'p', '--.-':  'q', '.-.':   'r',
+         '...':   's', '-':     't', '..-':   'u',
+         '...-':  'v', '.--':   'w', '-..-':  'x',
+         '-.--':  'y', '--..':  'z', '-----': '0',
+         '.----': '1', '..---': '2', '...--': '3',
+         '....-': '4', '.....': '5', '-....': '6',
+         '--...': '7', '---..': '8', '----.': '9'
+        }
+
+def morse_decoder(code):
+    #replace this for solution
+
+    splitted = code.split("   ")
+    for i in range(len(splitted)):
+        word = ""
+        for j in splitted[i].split(" "):
+            word += MORSE[j]
+        splitted[i] = word
+    return " ".join(splitted).capitalize()
+
+if __name__ == '__main__':
+    print("Example:")
+    print(morse_decoder('... --- ...'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+    assert morse_decoder("..--- ----- .---- ---..") == "2018"
+    assert morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--") == "It was a good day"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
