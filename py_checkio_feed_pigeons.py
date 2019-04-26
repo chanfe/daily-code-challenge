81fabd4fdd28b28375f2113e0a00ffce848da106
Felix Chan <felixchan29@gmail.com>
2019-04-25 23:40:05 -0400

    feed

--- py_checkio_feed_pigeons.py
+++ py_checkio_feed_pigeons.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     assert checkio(1) == 1, "1st example"
     assert checkio(2) == 1, "2nd example"
     assert checkio(5) == 3, "3rd example"
-    assert checkio(10) == 6, "4th example"
+    assert checkio(10) == 6, "4th example" 
